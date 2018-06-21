d9d01c769d18a5fc1abb90cafe5e1e755fa0a763
Thomas Madej <tom@madej.ca>
2018-06-20 21:36:57 -0400

    updated digitalbits.toml issues.

--- services/horizon/internal/db2/assets/asset_stat_test.go
+++ services/horizon/internal/db2/assets/asset_stat_test.go
@@ -18,7 +18,7 @@ func TestAssetsStatsQExec(t *testing.T) {
 		Amount:      1009876000,
 		NumAccounts: 1,
 		Flags:       1,
-		Toml:        "https://test.com/.well-known/stellar.toml",
+		Toml:        "https://test.com/.well-known/digitalbits.toml",
 	}
 
 	item1 := AssetStatsR{
@@ -40,7 +40,7 @@ func TestAssetsStatsQExec(t *testing.T) {
 		Amount:      3000010434000,
 		NumAccounts: 2,
 		Flags:       1,
-		Toml:        "https://test.com/.well-known/stellar.toml",
+		Toml:        "https://test.com/.well-known/digitalbits.toml",
 	}
 
 	testCases := []struct {
--- services/horizon/internal/ingest/asset_stat_test.go
+++ services/horizon/internal/ingest/asset_stat_test.go
@@ -141,11 +141,11 @@ func TestStatAccountInfo(t *testing.T) {
 		{
 			"GCXKG6RN4ONIEPCMNFB732A436Z5PNDSRLGWK7GBLCMQLIFO4S7EYWVU",
 			0,
-			"https://example.com/.well-known/stellar.toml",
+			"https://example.com/.well-known/digitalbits.toml",
 		}, {
 			"GA5WBPYA5Y4WAEHXWR2UKO2UO4BUGHUQ74EUPKON2QHV4WRHOIRNKKH2",
 			1,
-			"https://abc.com/.well-known/stellar.toml",
+			"https://abc.com/.well-known/digitalbits.toml",
 		}, {
 			"GBXGQJWVLWOYHFLVTKWV5FGHA3LNYY2JQKM7OAJAUEQFU6LPCSEFVXON",
 			2,
