1eecfdac36e313df7ee38c9cbebf32f3a9133f03
Volodymyr Pohyla <chromelviv@gmail.com>
2018-07-26 21:52:45 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 #if [ $BUILD_FROM_SRC -gt 0 ]
 #then
   # deploy horizon binary
-mkdir -p /go/src/github.com/digitalbits/go \
+sudo mkdir -p /go/src/github.com/digitalbits/go \
     && git clone --depth 1 --branch master git@github.com:DigitalBitsOrg/go.git /go/src/github.com/digitalbits/go \
     && cd /go/src/github.com/digitalbits/go \
     && curl https://glide.sh/get | sh \
@@ -13,26 +13,26 @@ mkdir -p /go/src/github.com/digitalbits/go \
 #    && go install github.com/digitalbits/go/services/horizon
 
  ll /go/bin/bifrost
- mv /go/bin/bifrost /usr/local/bin \
-  && chmod +x /usr/local/bin/bifrost
+sudo mv /go/bin/bifrost /usr/local/bin \
+  && sudo chmod +x /usr/local/bin/bifrost
 #fi
 #  mv /go/bin/federation /usr/local/bin \
 #  && chmod +x /usr/local/bin/federation
     
 #  mv /go/bin/horizon /usr/local/bin \
 #  && chmod +x /usr/local/bin/horizon
-apt-get update -y
-apt-get install ruby-dev build-essential -y
+sudo apt-get update -y
+sudo apt-get install ruby-dev build-essential -y
 gem install fpm
 
 echo "Create deb package..."
 
-fpm -s dir -t deb -C /usr/local/bin/bifrost --name bifrost --version 0.1.0 --iteration 1 --depends debian_dependency1 --description "Digitalbits-bifrost" .
+sudo fpm -s dir -t deb -C /usr/local/bin/bifrost --name bifrost --version 0.1.0 --iteration 1 --depends debian_dependency1 --description "Digitalbits-bifrost" .
 #fpm -s dir -t deb -C /usr/local/bin/federation --name federation --version 0.1.0 --iteration 1 --depends debian_dependency1 --description "Digitalbits-federation" .
 #fpm -s dir -t deb -C /usr/local/bin/horizon --name horizon --version 0.1.0 --iteration 1 --depends debian_dependency1 --description "Digitalbits-horizon" .
 echo "Create rpm package..."
 
-fpm -s dir -t rpm -C /usr/local/bin/bifrost --name bifrost --version 0.1.0 --iteration 1 --depends  redhat_dependency1 --description "digitalbits-bifrost" .
+sudo fpm -s dir -t rpm -C /usr/local/bin/bifrost --name bifrost --version 0.1.0 --iteration 1 --depends  redhat_dependency1 --description "digitalbits-bifrost" .
 #fpm -s dir -t rpm -C /usr/local/bin/federation --name federation --version 0.1.0 --iteration 1 --depends  redhat_dependency1 --description "digitalbits-federation" .
 #fpm -s dir -t rpm -C /usr/local/bin/horizon --name horizon --version 0.1.0 --iteration 1 --depends  redhat_dependency1 --description "digitalbits-horizon" .
 echo "deploying to Cloudsmith with cloudsmith-cli"
