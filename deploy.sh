77791b9cd03863d712f47e9e885f195cf720fa1d
Volodymyr Pohyla <chromelviv@gmail.com>
2018-07-27 13:22:51 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -22,11 +22,11 @@ sudo apt-get install ruby-dev build-essential -y
 gem install fpm
 
 echo "Create deb package..."
-fpm --verbose -s dir -t deb -C /usr/local/bin/bifrost --name bifrost --version 0.1.0 --iteration 1 --depends debian_dependency1 --description "Digitalbits-bifrost" .
+fpm --verbose -s dir -t deb -C /tmp/bifrost --name bifrost --version 0.1.0 --iteration 1 --depends debian_dependency1 --description "Digitalbits-bifrost" .
 
 echo "Create rpm package..."
 
-fpm --verbose -s dir -t rpm -C ~/bifrost --name bifrost --version 0.1.0 --iteration 1 --depends  redhat_dependency1 --description "digitalbits-bifrost" .
+fpm --verbose -s dir -t rpm -C /tmp/bifrost --name bifrost --version 0.1.0 --iteration 1 --depends  redhat_dependency1 --description "digitalbits-bifrost" .
 
 echo "deploying to Cloudsmith with cloudsmith-cli"
 
