7b954d3ac26043e6ce764e702b68cea33ab7f94d
bpigadip <tabachuks@mail.ru>
2018-07-27 16:42:59 +0300

    testing new ci file

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 apt-get update -y
-apt-get install ruby-dev build-essential -y
+apt-get install ruby-dev build-essential rpm -y
 gem install fpm
 
+mkdir -p $GOPATH/fpm && cd $GOPATH/fpm
 echo "Create deb package..."
-fpm --verbose -s dir -t deb -n bifrost --version 0.1.0 --iteration 1 --description "Digitalbits-bifrost" $GOPATH/bin/bifrost=/usr/local/bin
+fpm --verbose -s dir -t deb -n bifrost --version 0.1.1 --iteration 1 --description "Digitalbits-bifrost" $GOPATH/bin/bifrost=/usr/local/bin
 
 echo "Create rpm package..."
-fpm --verbose -s dir -t rpm -n bifrost --version 0.1.0 --iteration 1 --description "Digitalbits-bifrost" $GOPATH/bin/bifrost=/usr/local/bin
+fpm --verbose -s dir -t rpm -n bifrost --version 0.1.1 --iteration 1 --description "Digitalbits-bifrost" $GOPATH/bin/bifrost=/usr/local/bin
 
 echo "deploying to Cloudsmith with cloudsmith-cli"
 
 ls
-cloudsmith push deb digitalbits/dbtest/ubuntu/trusty bifrost_0.1.0-1_amd64.deb
-cloudsmith push rpm digitalbits/dbtest/el/7 bifrost-0.1.0-1.x86_64.rpm 
+cloudsmith push deb digitalbits/dbtest/ubuntu/trusty bifrost_0.1.1-1_amd64.deb
+cloudsmith push rpm digitalbits/dbtest/el/7 bifrost-0.1.1-1.x86_64.rpm
