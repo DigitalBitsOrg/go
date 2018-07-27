d09c16005423241965f38fccc3220bf8032830db
bpigadip <tabachuks@mail.ru>
2018-07-27 16:22:00 +0300

    testing new ci file

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ apt-get install ruby-dev build-essential -y
 gem install fpm
 
 echo "Create deb package..."
-fpm --verbose -s dir -t deb -C $GOPATH/bifrost --name bifrost --version 0.1.0 --iteration 1 --depends debian_dependency1 --description "Digitalbits-bifrost" .
+fpm --verbose -s file -t deb -C $GOPATH/bifrost/bifrost --name bifrost --version 0.1.0 --iteration 1 --depends debian_dependency1 --description "Digitalbits-bifrost" .
 
 echo "Create rpm package..."
-fpm --verbose -s dir -t rpm -C $GOPATH/bifrost --name bifrost --version 0.1.0 --iteration 1 --depends  redhat_dependency1 --description "digitalbits-bifrost" .
+fpm --verbose -s file -t rpm -C $GOPATH/bifrost/bifrost --name bifrost --version 0.1.0 --iteration 1 --depends  redhat_dependency1 --description "digitalbits-bifrost" .
 
 echo "deploying to Cloudsmith with cloudsmith-cli"
 
