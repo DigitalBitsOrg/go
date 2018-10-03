798156fdb816d061b8b0361e6563083661f3441b
Thomas Madej <tom@madej.ca>
2018-10-03 17:19:19 -0400

    updated travis config & deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
+tag=$1
+iteration=$2
+version=${tag:1}
+
+echo $tag
+echo $iteration
+echo $version
+
 sudo apt-get update -y
 sudo apt-get install ruby-dev build-essential rpm -y
 gem install fpm
 
-mkdir -p $GOPATH/fpm && cd $GOPATH/fpm
 echo "Create deb package..."
-fpm --verbose -s dir -t deb -n frontier --version 0.0.3 --iteration 1 --description "Digitalbits-frontier" $GOPATH/bin/frontier=/usr/local/bin/frontier
-fpm --verbose -s dir -t deb -n bifrost --version 0.0.3 --iteration 1 --description "Digitalbits-bifrost" $GOPATH/bin/bifrost=/usr/local/bin/bifrost
-fpm --verbose -s dir -t deb -n federation --version 0.0.3 --iteration 1 --description "Digitalbits-federation" $GOPATH/bin/federation=/usr/local/bin/federation
+fpm --verbose -s dir -t deb -n frontier --version $version --iteration $iteration --description "DigitalBits Frontier Server" $GOPATH/bin/frontier=/usr/local/bin/frontier
+fpm --verbose -s dir -t deb -n bifrost --version $version --iteration $iteration --description "DigitalBits BiFrost Server" $GOPATH/bin/bifrost=/usr/local/bin/bifrost
+fpm --verbose -s dir -t deb -n federation --version $version --iteration $iteration --description "DigitalBits Federation Server" $GOPATH/bin/federation=/usr/local/bin/federation
 
 echo "Create rpm package..."
-fpm --verbose -s dir -t rpm -n frontier --version 0.0.3 --iteration 1 --description "Digitalbits-frontier" $GOPATH/bin/frontier=/usr/local/bin/frontier
-fpm --verbose -s dir -t rpm -n bifrost --version 0.0.3 --iteration 1 --description "Digitalbits-bifrost" $GOPATH/bin/bifrost=/usr/local/bin/bifrost
-fpm --verbose -s dir -t rpm -n federation --version 0.0.3 --iteration 1 --description "Digitalbits-federation" $GOPATH/bin/federation=/usr/local/bin/federation
+fpm --verbose -s dir -t rpm -n frontier --version $version --iteration $iteration --description "DigitalBits Frontier Server" $GOPATH/bin/frontier=/usr/local/bin/frontier
+fpm --verbose -s dir -t rpm -n bifrost --version $version --iteration $iteration --description "DigitalBits BiFrost Server" $GOPATH/bin/bifrost=/usr/local/bin/bifrost
+fpm --verbose -s dir -t rpm -n federation --version $version --iteration $iteration --description "DigitalBits Federation Server" $GOPATH/bin/federation=/usr/local/bin/federation
 
 echo "deploying to Cloudsmith with cloudsmith-cli"
 
 ls
-cloudsmith push deb digitalbits/dbtest/ubuntu/trusty frontier.0.3-1_amd64.deb
-cloudsmith push deb digitalbits/dbtest/ubuntu/trusty bifrost_0.0.3-1_amd64.deb
-cloudsmith push deb digitalbits/dbtest/ubuntu/trusty federation_0.0.3-1_amd64.deb
+cloudsmith push deb digitalbits/dbtest/ubuntu/trusty frontier-$version-$iteration.amd64.deb
+cloudsmith push deb digitalbits/dbtest/ubuntu/trusty bifrost-$version-$iteration.amd64.deb
+cloudsmith push deb digitalbits/dbtest/ubuntu/trusty federation-$version-$iteration.amd64.deb
 
-cloudsmith push rpm digitalbits/dbtest/el/7 frontier-0.0.3-1.x86_64.rpm
-cloudsmith push rpm digitalbits/dbtest/el/7 bifrost-0.0.3-1.x86_64.rpm
-cloudsmith push rpm digitalbits/dbtest/el/7 federation-0.0.3-1.x86_64.rpm
+cloudsmith push rpm digitalbits/dbtest/el/7 frontier-$version-$iteration.x86_64.rpm
+cloudsmith push rpm digitalbits/dbtest/el/7 bifrost-$version-$iteration.x86_64.rpm
+cloudsmith push rpm digitalbits/dbtest/el/7 federation-$version-$iteration.x86_64.rpm
